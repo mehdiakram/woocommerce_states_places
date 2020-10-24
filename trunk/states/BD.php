ccac056e95fdf9eda301773aaa833690e2c9717b
SM Mehdi Akram <56603254+mehdiakram@users.noreply.github.com>
2020-10-24 08:51:58 +0600

    Add files via upload
    Bangladesh

---
+++ trunk/states/BD.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Localities of Bangladesh
+ * 
+ * @author   S.M. Mehdi Akram <https://www.royaltechbd.com>
+ * @version  1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @credit   mehdi@royaltechbd.com <https://www.royaltechbd.com>
+ */
+
+global $states;
+
+$states['BD'] = array(
+	'Dhaka' => __('Dhaka', 'states-cities-and-places-for-woocommerce'),
+	'Faridpur' => __('Faridpur', 'states-cities-and-places-for-woocommerce'),
+	'Gazipur' => __('Gazipur', 'states-cities-and-places-for-woocommerce'),
+	'Gopalganj' => __('Gopalganj', 'states-cities-and-places-for-woocommerce'),
+	'Kishoreganj' => __('Kishoreganj', 'states-cities-and-places-for-woocommerce'),
+	'Madaripur' => __('Madaripur', 'states-cities-and-places-for-woocommerce'),
+	'Manikganj' => __('Manikganj', 'states-cities-and-places-for-woocommerce'),
+	'Munshiganj' => __('Munshiganj', 'states-cities-and-places-for-woocommerce'),
+	'Narayanganj' => __('Narayanganj', 'states-cities-and-places-for-woocommerce'),
+	'Narsingdi' => __('Narsingdi', 'states-cities-and-places-for-woocommerce'),
+	'Rajbari' => __('Rajbari', 'states-cities-and-places-for-woocommerce'),
+	'Shariatpur' => __('Shariatpur', 'states-cities-and-places-for-woocommerce'),
+	'Tangail' => __('Tangail', 'states-cities-and-places-for-woocommerce'),
+	'Barisal' => __('Barisal', 'states-cities-and-places-for-woocommerce'),
+	'Barguna' => __('Barguna', 'states-cities-and-places-for-woocommerce'),
+	'Bhola' => __('Bhola', 'states-cities-and-places-for-woocommerce'),
+	'Jhalakati' => __('Jhalakati', 'states-cities-and-places-for-woocommerce'),
+	'Patuakhali' => __('Patuakhali', 'states-cities-and-places-for-woocommerce'),
+	'Pirojpur' => __('Pirojpur', 'states-cities-and-places-for-woocommerce'),
+	'Bandarban' => __('Bandarban', 'states-cities-and-places-for-woocommerce'),
+	'Brahmanbaria' => __('Brahmanbaria', 'states-cities-and-places-for-woocommerce'),
+	'Chandpur' => __('Chandpur', 'states-cities-and-places-for-woocommerce'),
+	'Chittagong' => __('Chittagong', 'states-cities-and-places-for-woocommerce'),
+	'Comilla' => __('Comilla', 'states-cities-and-places-for-woocommerce'),
+	'Coxs-Bazar' => __('Coxs Bazar', 'states-cities-and-places-for-woocommerce'),
+	'Feni' => __('Feni', 'states-cities-and-places-for-woocommerce'),
+	'Khagrachhari' => __('Khagrachhari', 'states-cities-and-places-for-woocommerce'),
+	'Lakshmipur' => __('Lakshmipur', 'states-cities-and-places-for-woocommerce'),
+	'Noakhali' => __('Noakhali', 'states-cities-and-places-for-woocommerce'),
+	'Rangamati' => __('Rangamati', 'states-cities-and-places-for-woocommerce'),
+	'Jamalpur' => __('Jamalpur', 'states-cities-and-places-for-woocommerce'),
+	'Mymensingh' => __('Mymensingh', 'states-cities-and-places-for-woocommerce'),
+	'Netrakona' => __('Netrakona', 'states-cities-and-places-for-woocommerce'),
+	'Sherpur' => __('Sherpur', 'states-cities-and-places-for-woocommerce'),
+	'Bagerhat' => __('Bagerhat', 'states-cities-and-places-for-woocommerce'),
+	'Chuadanga' => __('Chuadanga', 'states-cities-and-places-for-woocommerce'),
+	'Jessore' => __('Jessore', 'states-cities-and-places-for-woocommerce'),
+	'Jhenaidah' => __('Jhenaidah', 'states-cities-and-places-for-woocommerce'),
+	'Khulna' => __('Khulna', 'states-cities-and-places-for-woocommerce'),
+	'Kushtia' => __('Kushtia', 'states-cities-and-places-for-woocommerce'),
+	'Magura' => __('Magura', 'states-cities-and-places-for-woocommerce'),
+	'Meherpur' => __('Meherpur', 'states-cities-and-places-for-woocommerce'),
+	'Narail' => __('Narail', 'states-cities-and-places-for-woocommerce'),
+	'Satkhira' => __('Satkhira', 'states-cities-and-places-for-woocommerce'),
+	'Bogra' => __('Bogra', 'states-cities-and-places-for-woocommerce'),
+	'Chapai-Nawabganj' => __('Chapai Nawabganj', 'states-cities-and-places-for-woocommerce'),
+	'Jaipurhat' => __('Jaipurhat', 'states-cities-and-places-for-woocommerce'),
+	'Naogaon' => __('Naogaon', 'states-cities-and-places-for-woocommerce'),
+	'Natore' => __('Natore', 'states-cities-and-places-for-woocommerce'),
+	'Pabna' => __('Pabna', 'states-cities-and-places-for-woocommerce'),
+	'Rajshahi' => __('Rajshahi', 'states-cities-and-places-for-woocommerce'),
+	'Sirajganj' => __('Sirajganj', 'states-cities-and-places-for-woocommerce'),
+	'Dinajpur' => __('Dinajpur', 'states-cities-and-places-for-woocommerce'),
+	'Gaibandha' => __('Gaibandha', 'states-cities-and-places-for-woocommerce'),
+	'Kurigram' => __('Kurigram', 'states-cities-and-places-for-woocommerce'),
+	'Lalmonirhat' => __('Lalmonirhat', 'states-cities-and-places-for-woocommerce'),
+	'Nilphamari' => __('Nilphamari', 'states-cities-and-places-for-woocommerce'),
+	'Panchagarh' => __('Panchagarh', 'states-cities-and-places-for-woocommerce'),
+	'Rangpur' => __('Rangpur', 'states-cities-and-places-for-woocommerce'),
+	'Thakurgaon' => __('Thakurgaon', 'states-cities-and-places-for-woocommerce'),
+	'Habiganj' => __('Habiganj', 'states-cities-and-places-for-woocommerce'),
+	'Maulvi-Bazar' => __('Maulvi Bazar', 'states-cities-and-places-for-woocommerce'),
+	'Sunamganj' => __('Sunamganj', 'states-cities-and-places-for-woocommerce'),
+	'Sylhet' => __('Sylhet', 'states-cities-and-places-for-woocommerce'),
+	'Saver' => __('Saver', 'states-cities-and-places-for-woocommerce'),
+
+
+);
